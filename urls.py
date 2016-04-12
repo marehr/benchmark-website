7835b0b11b28a45d57df032153e5c3ef3c5a18f0
marehr <marehr@zedat.fu-berlin.de>
2016-04-12 15:07:34 +0200

    Imported layout of benchmark

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^compare/(?P<benchmark_id1>[0-9]+)/(?P<benchmark_id2>[0-9]+)', views.compare, name='compare'),
+    url(r'^upload', views.upload, name='upload'),
+]
--- views.py
+++ views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
 
-# Create your views here.
+# from .models import Benchmark
+
+def compare(request, benchmark_id1, benchmark_id2):
+    # benchmark1 = get_object_or_404(Benchmark, pk=benchmark_id1)
+    # benchmark2 = get_object_or_404(Benchmark, pk=benchmark_id2)
+    context = {}
+    return render(request, 'benchmark_app/compare.html', context)
+
+def upload(request):
+    context = {}
+    return render(request, 'benchmark_app/upload.html', context)
+
+def home(request):
+    context = {}
+    return render(request, 'benchmark_app/home.html', context)
